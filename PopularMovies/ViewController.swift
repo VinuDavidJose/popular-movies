e5da658e39075497e2fe882275b5f90bee620ddf
Vinu David Jose <vinudavidjose@gmail.com>
2018-05-01 12:33:54 +0530

    Initial Commit

---
+++ PopularMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopularMovies
+//
+//  Created by David Jose on 01/05/18.
+//  Copyright © 2018 VinuDavidJose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
